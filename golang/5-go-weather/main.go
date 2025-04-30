9334660232ada55b3bfe7b40e51302b0e1694e7e
bernar.berdikul <b.berdikul@mystartups.kz>
2025-04-30 19:29:28 +0500

    Add CLI parsers

--- golang/5-go-weather/main.go
+++ golang/5-go-weather/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	city := flag.String("city", "", "person's city")
+	format := flag.Int("format", 1, "Output weather format")
+
+	flag.Parse()
+
+	fmt.Println(*city)
+	fmt.Println(*format)
 }
