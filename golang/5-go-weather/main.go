d568aacc4e9c7f91d8603e37fed0189f520950aa
bernar_berdicool <Dabeerman32215@gmail.com>
2025-05-05 00:46:13 +0500

    5-go-weather: Add data reader.

--- golang/5-go-weather/main.go
+++ golang/5-go-weather/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"strings"
 )
 
 func main() {
@@ -13,4 +15,14 @@ func main() {
 
 	fmt.Println(*city)
 	fmt.Println(*format)
+
+	reader := strings.NewReader("Hello, I'm data stream")
+	block := make([]byte, 4)
+	for {
+		_, err := reader.Read(block)
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("%q\n", block)
+	}
 }
